369557a7f4ec30619920273c3b9d083b4d66de40
isisj <1078160@hr.nl>
2025-04-13 19:36:00 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+/** @type {import('vite').UserConfig} */
+
+// https://vitejs.dev/config
+
+export default defineConfig( {
+    base: "./",
+
+    build: {
+        
+    }
+})
